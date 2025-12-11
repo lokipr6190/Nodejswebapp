ffa874c03334b6d3de372b341da1ddcd0dba3c94
lokipr6190 <76768106+lokipr6190@users.noreply.github.com>
2025-12-12 00:04:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/' , (req,res) => {
-    res.send('Hello Lokesh');
+    res.send('Hello Lokesh V2');
 });
 
 app.listen(8080, () => {
     console.log('Listening on port 8080');
 })
+
